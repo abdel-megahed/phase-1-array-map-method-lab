0f3742c9bdba9ccc67f29b04fd298d5126be01f5
abdel <ammegahe@amazon.com>
2023-04-02 08:21:40 -0400

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+function titleCase(str) {
+  return str
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
+  'Implementing Blockchain Web API',
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
@@ -11,6 +17,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial) {
+    return tutorial.split(' ').map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
